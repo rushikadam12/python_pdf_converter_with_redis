7567d63f45ba291efdb4ad6de86cf4f9f03b73bd
rushikadam12 <rushi55578@gmail.com>
2024-09-04 22:24:34 +0530

    fix the pdf binray bug

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,12 @@
-from flask import Blueprint, request, jsonify, session, make_response
+from flask import (
+    Blueprint,
+    request,
+    jsonify,
+    session,
+    Response,
+    make_response,
+    send_file,
+)
 from fpdf import FPDF
 import uuid
 import os
@@ -10,6 +18,7 @@ from app import redis_client
 import json
 import base64
 import io
+import zipfile
 
 main_dp = Blueprint("main_dp", __name__)
 load_dotenv()
@@ -36,6 +45,7 @@ def cache_file(cache_key, file_name, file_bytes):
         cache_key,
         mapping={
             "file_name": file_name,
+            "converted_file": "",
             "file_bytes": base64.b64encode(file_bytes).decode("utf-8"),
         },
     )
@@ -59,9 +69,11 @@ def file_upload():
         # print(hash_values)
         file_name = hash_values[b"file_name"].decode("utf-8")
         file_bytes = base64.b64encode(hash_values[b"file_bytes"]).decode("utf-8")
-        response = make_response({"file_name": file_name, "file_bytes": file_bytes, "message": "cache file"})
+        response = make_response(
+            {"file_name": file_name, "file_bytes": file_bytes, "message": "cache file"}
+        )
 
-        response.set_cookie("file_id", file_hash, httponly=True, secure=True)
+        # response.set_cookie("file_id", file_hash, httponly=True, secure=True)
         return response
 
     file_bytes = User_file.read()  # convert into bytes
@@ -90,31 +102,43 @@ def convert_xlsx():
     try:
 
         file_hash = request.cookies.get("file_id")
+        session_id = session.get("userId")
         if file_hash is None:
             return jsonify({"message": "file not found pls check the file"})
+
         file_data = redis_client.hgetall(file_hash)
-
         file_name = file_data[b"file_name"].decode("utf-8")
+        file_extension = file_name.split(".")
         file_bytes = base64.b64decode(file_data[b"file_bytes"])
-        read_file = io.BytesIO(file_bytes)
+        file_like = io.BytesIO(file_bytes)
 
-        df = pd.read_excel(read_file, engine="openpyxl")
-        print(df)
-        return {"file_name": file_name, "file_bytes": str(file_bytes)}
-        #####################################################
-        return ""
+        # df = pd.read_excel(read_file, engine="openpyxl")
+        # print(df)
+        # return {"file_name": file_name, "file_bytes": str(file_bytes)}
+        # if file_bytes is not None:
+        #     return send_file(
+        #         file_like,
+        #         mimetype="application/pdf",
+        #         as_attachment=True,
+        #         download_name=f"{file_hash}.pdf",
+        #     )
+
+        # return " hey"
+
         # for old file format '.xls'
-        if file_extension == ".xls":
 
+        if "xls" == file_extension[1]:
             df = pd.read_excel(
-                file_path,
+                file_bytes,
                 engine="xlrd",
             )
         # for new file format 'xlsx'
-        else:
 
-            df = pd.read_excel(file_path, engine="openpyxl")
+        elif "xlsx" == file_extension[1]:
+            df = pd.read_excel(file_bytes, engine="openpyxl")
             print("i execute")
+        else:
+            return {"message": "unsupported file type"}
 
         pdf = FPDF()
 
@@ -138,15 +162,53 @@ def convert_xlsx():
                 print(f"Writing value: {row_text} (type: {type(value)})")
                 pdf.cell(columns_widths[i], 10, txt=row_text, border=1, align="c")
             pdf.ln()
+        #################################################################
+        file_split_name = file_name.split(".")
 
-        output_pdf_file = os.path.join(
-            os.environ.get("STORAGE_PATH"), f"{session_id}.pdf"
-        )
-        pdf.output(output_pdf_file)
+        # output_pdf_file = os.path.join(file_name[0], f"{session_id}.pdf")
 
+        # save the pdf buffer to memory
+        pdf_Buffer = io.BytesIO()
+        pdf.output(pdf_Buffer)
+        pdf_Buffer.seek(0)  # go to the start of the buffer to read
+
+        pdf_bytes = pdf_Buffer.read()
+        if not pdf_bytes:
+            return "not able to read pdf bytes"
+
+        # base64_pdf_encoding=base64.b64encode(pdf_bytes).decode("utf-8")
         print("file os converted ")
 
-        return jsonify({"message": "file converted successfully", "success": True}), 200
+        redis_client.hset(file_hash, "converted_file", pdf_bytes)
+
+        # cache values
+        hash_values = redis_client.hget(file_hash,'converted_file')
+        if hash_values:
+
+            # decoded_file = io.BytesIO(hash_values)
+            # decoded_file.seek(0)
+            # decoded_read_value = decoded_file.read()
+            # print(decoded_read_value)
+
+            # with open("debug_file.pdf","wb") as f1 ,open("debug_retrieved.pdf","wb") as f2:
+            #     f1.write(pdf_bytes)
+            #     f2.write(decoded_file)
+
+            # file_like = io.BytesIO(decoded_file)
+            # file_like.seek(0)
+            response = Response(hash_values, content_type="application/pdf")
+            response.headers["Content-Disposition"] = (
+                'attachment;filename="converted_file.pdf"'
+            )
+            return response
+        else:
+            return "something went wrong"
+        # return send_file(
+        #     response_file,
+        #     mimetype="application/pdf",
+        #     as_attachment=True,
+        #     download_name=f"{file_split_name[0]}.pdf",
+        # )
 
     except Exception as e:
         return str(e)
